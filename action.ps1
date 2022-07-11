439982e00acbb252142d20d2703b4b5e11d713a7
Rob Cannon <rcannon@acceleratelearning.com>
2022-07-11 12:21:48 -0400

    Specifiying inputs as environment variables

--- action.ps1
+++ action.ps1
@@ -7,7 +7,7 @@ param (
     [String]$ReleaseHtmlUrl = $env:INPUT_RELEASE_HTML_URL
 )
 
-Get-ChildItem env: | Sort-Object
+Get-ChildItem env: | Sort-Object name
 
 if (-Not $WebhookUrl) {
     exit 0
